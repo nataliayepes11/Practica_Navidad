e5ee6d14b77fc7a9c2c75c307cce60e20965808e
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-21 01:35:38 -0300

    CONST/LET + IF/ELSE

--- Ej.C9.js
+++ Ej.C9.js
@@ -5,3 +5,15 @@ Ejercicio: El outfit de Año Nuevo 👗👔 Tenemos que decidir qué ponernos se
 2.	Si la temperatura es mayor a 25 grados, mostrá: "Ponete ropa blanca y liviana, ¡hace calor! ☀️".
 3.	Si hace 25 grados o menos, mostrá: "Llevá un abrigo elegante por las dudas 🧥".
 */
+
+const prompt = require (`prompt-sync`)();
+const TEMPERATURA = 25;
+let outfit = parseFloat(prompt("Ingresá la temperatura actual (sólo el número): "));
+
+
+if (outfit > TEMPERATURA) {
+    console.log("Ponete ropa blanca y liviana");
+  
+} else {
+  console.log("Llevá un abrigo elegante por las dudas");
+}
